e9f2905bcdf52f38c1a34dd53086bba160c91210
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2023-10-27 22:00:24 -0300

    test: adds test threshholds

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,11 @@ export default defineConfig({
     coverage: {
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['text', 'json-summary', 'json'],
+
+      statements: 90,
+      functions: 90,
+      branches: 90,
+      lines: 90,
     }
   }
 });
